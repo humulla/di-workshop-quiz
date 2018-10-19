3e3d3df08e95908978f1c0fcc221c5ca4f45d1df
humairaa_mulla@hotmail.com <humairaa.mulla@hackney.gov.uk>
2018-10-19 13:59:09 +0100

    workshop

--- quiz.js
+++ quiz.js
@@ -1,3 +1,34 @@
+var score = 0;
+var username;
+var QuestionOne = "Berlin is the capital of which country? a)Germany, b)Belgium c) Netherlands"
+var AnswerOne = "a"
+var userAnswerOne; 
+
+
+
 function startGame() {
-    alert('starting!')
+    // get the user's name
+    username = prompt("Welcome to the General quiz, What is your name?")
+    AskQuestion(QuestionOne, AnswerOne)
+//     alert(username) 
+//     userAnswerOne = prompt("Welcome, " +username+". Your first question is:" + QuestionOne);
+//     if (userAnswerOne == AnswerOne ){
+//      score++
+//     } else{
+//         alert("The answer was " +AnswerOne)
+
+//     }
+// console.log (userAnswerOne, AnswerOne)
+
 }
+
+function AskQuestion(question, correctAnswer) {
+    var userAnswer = prompt(question);
+    if(userAnswer.toLowerCase == correctAnswer.toLowerCase()) {
+       score++
+    } else {
+        alert("The answer was" +correctAnswer.toLowerCase)
+    }
+
+} // this is the end of the AskQuestion function
+console.log(score)
