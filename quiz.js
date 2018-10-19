e856c4472d5bc8d742914e180093b7370d05d2de
humairaa_mulla@hotmail.com <humairaa.mulla@hackney.gov.uk>
2018-10-19 15:31:36 +0100

    improve quiz

--- quiz.js
+++ quiz.js
@@ -10,6 +10,8 @@ function startGame() {
     // get the user's name
     username = prompt("Welcome to the General quiz, What is your name?")
     AskQuestion(QuestionOne, AnswerOne)
+    AskQuestion("Does the moon have gravity? a)Yes its 1.622 m/s b)No there is no gravity where you float", "a") 
+    AskQuestion("You want to make green paint. Which colours do you miss together? a) red and yellow b) orange and purple c) blue and yellow", "b")
 //     alert(username) 
 //     userAnswerOne = prompt("Welcome, " +username+". Your first question is:" + QuestionOne);
 //     if (userAnswerOne == AnswerOne ){
