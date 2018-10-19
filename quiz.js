2e915b174abf579a199cfebbf569745ef39b18a8
humairaa_mulla@hotmail.com <humairaa.mulla@hackney.gov.uk>
2018-10-19 15:41:18 +0100

    include arrays

--- quiz.js
+++ quiz.js
@@ -4,14 +4,15 @@ var QuestionOne = "Berlin is the capital of which country? a)Germany, b)Belgium 
 var AnswerOne = "a"
 var userAnswerOne; 
 
-
-
+var questions = ["Berlin is the capital of which country? a)Germany, b)Belgium c) Netherlands", "Does the moon have gravity? a)Yes its 1.622 m/s b)No there is no gravity where you float", "You want to make green paint. Which colours do you miss together? a) red and yellow b) orange and purple c) blue and yellow"]
+var answers = ["a", "a", "b"]
 function startGame() {
     // get the user's name
     username = prompt("Welcome to the General quiz, What is your name?")
-    AskQuestion(QuestionOne, AnswerOne)
-    AskQuestion("Does the moon have gravity? a)Yes its 1.622 m/s b)No there is no gravity where you float", "a") 
-    AskQuestion("You want to make green paint. Which colours do you miss together? a) red and yellow b) orange and purple c) blue and yellow", "b")
+    AskQuestion(questions[0], answers[0])
+    AskQuestion(questions[1], answers[1]) 
+    AskQuestion(questions[2], answers[2])
+    
 //     alert(username) 
 //     userAnswerOne = prompt("Welcome, " +username+". Your first question is:" + QuestionOne);
 //     if (userAnswerOne == AnswerOne ){
